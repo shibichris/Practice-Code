f2b8ce424063cdf235e82cd6a22ae1ceb94266a6
shibichris <99465215+shibichris@users.noreply.github.com>
2022-03-14 11:32:54 +0530

    Spring AOP

---
+++ demo/Main.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+@Component
+public class Main {
+	@Autowired
+
+	public static void main(String[] args) {
+		
+	}
+}
