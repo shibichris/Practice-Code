1e2b4bbbd3e783d5e4647fd1f671797b27901593
shibichris <99465215+shibichris@users.noreply.github.com>
2022-03-24 09:44:52 +0530

    Abstraction

---
+++ abstraction/Abstraction.java
@@ -0,0 +1,40 @@
+package com.ood.concepts.abstraction;
+
+class Person {
+
+	private double id;
+	private String name;
+
+	public Person() {
+
+		id = Math.random();
+		sayHello();
+	}
+
+	
+	private void sayHello() {
+		System.out.println("Hello - " + getId());
+	}
+
+	public double getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
+
+
+public class Abstraction {
+
+	public static void main(String[] args) {
+
+		Person p1 = new Person();
+		p1.setName("Tirthal");
+		System.out.println("Person 1 - " + p1.getId() + " : " + p1.getName());
+	}
+}
---
+++ abstraction/FullTimeEmployee.java
@@ -0,0 +1,10 @@
+package com.ood.concepts.abstraction;
+public class FullTimeEmployee extends Employee {
+	public FullTimeEmployee(String name, int paymentPerHour) {
+		super(name, paymentPerHour);
+	}
+	@Override
+	public int calculateSalary() {
+		return getPaymentPerHour() * 8;
+	}
+}
